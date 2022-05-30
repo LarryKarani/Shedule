c388b580a5a920a2822da0cf1bbc7b3c4fdc29ba
LarryKarani <karanilarry@gmail.com>
2022-05-30 13:04:24 +0300

    feature(Routing):Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Layout from "./components/Layout/layout";
+
 function App() {
   return (
-   <h1 className="text-3xl font-bold underline">
-     Hello world
-   </h1>
-  );
+    <div className='h-full bg-gray-100'>
+      <Layout/>
+    </div>
+  )
 }
 
 export default App;
